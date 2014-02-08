716ab5125b8e3660890b3c23ce168ddd894baf36
Lunarx <lunarx@Dev.(none)>
2014-02-08 02:33:52 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StatTracker::Application.config.secret_key_base = '0b326495d95de1862b11b4c0bea81076f887c04ea0ef3c91a1366d6470b398783dbb9e197271a14a9f8fafacb14e691aa07655399b6c424a1d46ba19b62ec8b0'
